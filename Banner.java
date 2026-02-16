fb02cea434f886f67078519dcf6c96f4b073c0d1
krishanth1207 <itskrishanth1207@gmail.com>
2026-02-16 09:24:03 +0530

    UC2: Display OOPS in large banner format using multiple print statements

--- Banner.java
+++ Banner.java
@@ -1,6 +1,12 @@
 public class Banner {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   *     *  *      ");
+        System.out.println("*     *  *     *   ******    ***** ");
+        System.out.println("*     *  *     *   *              *");
+        System.out.println("*     *  *     *   *        *     *");
+        System.out.println(" *****    *****    *         ***** ");
     }
 }
