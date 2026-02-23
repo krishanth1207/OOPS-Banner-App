233bcfaf6104326d1eb5eb771147eeb0647d3b8f
krishanth1207 <itskrishanth1207@gmail.com>
2026-02-23 08:48:17 +0530

    Refactor banner array to use bannerLines

--- Banner.java
+++ Banner.java
@@ -2,20 +2,19 @@ public class Banner {
 
     public static void main(String[] args) {
 
-        String[] banner = {
+        String[] bannerLines = new String[7];
 
-            String.join("  ", " ***** ", " ***** ", " ******", " ***** "),
-            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
-            String.join("  ", "*     *", "*     *", "*     *", "*      "),
-            String.join("  ", "*     *", "*     *", " ******", " ***** "),
-            String.join("  ", "*     *", "*     *", "*      ", "      *"),
-            String.join("  ", "*     *", "*     *", "*      ", "*     *"),
-            String.join("  ", " ***** ", " ***** ", "*      ", " ***** ")
+        bannerLines[0] = String.join("", "  ***       ***      ", "****      ", "****");
+        bannerLines[1] = String.join("", " *   *     *   *     ", "*   *    ", "*");
+        bannerLines[2] = String.join("", "*     *   *     *    ", "****      ", "****");
+        bannerLines[3] = String.join("", "*     *   *     *    ", "*            ", "*");
+        bannerLines[4] = String.join("", "*     *   *     *    ", "*            ", "*");
+        bannerLines[5] = String.join("", " *   *     *   *     ", "*        ", "*   *");
+        bannerLines[6] = String.join("", "  ***       ***      ", "*         ", "***");
 
-        };
-
-        for (String line : banner) {
+        for (String line : bannerLines) {
             System.out.println(line);
         }
     }
 }
+
